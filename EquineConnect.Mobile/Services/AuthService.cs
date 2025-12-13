fe6594c21a4bcf0d1f7397a6d21345da68e0f78b
Misslittle87 <92373716+Misslittle87@users.noreply.github.com>
2025-12-13 16:38:42 +0100

    Working login

--- EquineConnect.Mobile/Services/AuthService.cs
+++ EquineConnect.Mobile/Services/AuthService.cs
@@ -1,28 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http.Json;
-using System.Text;
+﻿using System.Net.Http.Json;
 
 namespace EquineConnect.Mobile.Services
 {
     public class AuthService
     {
-        private readonly HttpClient _httpClient = new HttpClient { BaseAddress = new Uri("http://10.0.2.2:5041/") };
+        private readonly HttpClient _httpClient;
 
         public AuthService(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
-        public async Task<string> Login(string email, string password)
+        public async Task<string?> Login(string email, string password)
         {
-            var response = await _httpClient.PostAsJsonAsync("api/auth/login", new { Email = email, Password = password });
+            var response = await _httpClient.PostAsJsonAsync(
+                "api/auth/login",
+                new
+                {
+                    email,
+                    password,
+                    rememberMe = true
+                });
 
             if (!response.IsSuccessStatusCode)
                 return null;
 
             var result = await response.Content.ReadFromJsonAsync<AuthResponse>();
-            return result.Token;
+            return result?.Token;
         }
     }
 }
